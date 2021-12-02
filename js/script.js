3112bd276961e911aec1049eab5af3dac7b20d86
Albaer33 <alisi3396@gmail.com>
2021-12-02 13:42:45 +0100

    fine esercizio

---
+++ js/script.js
@@ -0,0 +1,12 @@
+Vue.config.devtools = true;
+
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+            name: 'alberto',
+            lastname: 'lisi',
+            img: "img/logo.png"
+        }
+    }
+);
